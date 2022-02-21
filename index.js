554d66c43b98cd0898ae5f14d0b5809c3b271270
Yasmin <yasmin.bunnie@gmail.com>
2022-02-21 19:46:19 +0000

    used inquirer to get user answers and deleted file duplication

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-  console.log("hello World");
+  inquirer.prompt(questions).then((inquirerAnswers) => {
+    // Creating variable to store string returned from GenerateMarkdown using Users console answers
+    const markdownData = generateMarkdown({ ...inquirerAnswers });
+    // Passing file name and the users answers string to writeToFile function
+    writeToFile("README.md", markdownData);
+  });
 }
 
 // Function call to initialize app
